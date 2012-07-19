aaaec404d5d3b57c16f397847b1615606e09eefa
e-ihrke <ihrke.e@gmail.com>
2012-07-19 11:22:30 +0200

    moved tests into an extra directory.

--- src/main/python/yum_repo_server/daemon/tests.py
+++ src/main/python/yum_repo_server/daemon/tests.py
@@ -2,4 +2,4 @@
 Django executes only test classes, that are imported in the tests.py files.
 Here are the imports for this module.
 '''
-from testStartWorkingSchedulerDaemon import TestStartWorkingSchedulerDaemon
+from test.testStartWorkingSchedulerDaemon import TestStartWorkingSchedulerDaemon
