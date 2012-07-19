06972bd97411e1f8c62870cf98f0eb6c2ffc7684
e-ihrke <ihrke.e@gmail.com>
2012-07-19 16:38:54 +0200

    added propagation of a rpm from one repository to another.

---
+++ src/main/python/yum_repo_server/api/handlers/rpmPropagationHandler.py
@@ -0,0 +1,50 @@
+from piston.handler import BaseHandler
+from piston.utils import rc
+from yum_repo_server.api.services.repoConfigService import RepoConfigService
+import os
+import shutil
+
+class RpmPropagationHandler(BaseHandler):
+    config = RepoConfigService()
+    
+    def create(self, request):
+        data = request.POST
+        
+        if not data.has_key('source') or not data.has_key('destination'):
+            return self._build_bad_response('source and destination attribute must be set.')
+        
+        try:             
+            source_repo, source_arch, rpm = data['source'].split('/')
+        except ValueError:
+            return self._build_bad_response('source format is not valid. (repo_name/architecture/rpm-name)')
+        
+        source_repo_path = self.config.getStaticRepoDir(source_repo)
+        
+        if not os.path.exists(source_repo_path):
+            return self._build_bad_response('source repository does not exist.')
+
+        destination_repo_path = self.config.getStaticRepoDir(data['destination'])
+        if not os.path.exists(destination_repo_path):
+            return self._build_bad_response('destination repository does not exist')
+        
+        source_rpm_path = os.path.join(source_repo_path, source_arch, rpm)
+        destination_rpm_path = os.path.join(destination_repo_path, source_arch, rpm)
+        if not os.path.exists(source_rpm_path):
+            return self._build_bad_response('rpm_file could not be found.')
+        
+        destination_rpm_parent_dir = os.path.dirname(destination_rpm_path)
+        if not os.path.exists(destination_rpm_parent_dir):
+            os.mkdir(destination_rpm_parent_dir)
+        
+        shutil.move(source_rpm_path, destination_rpm_path)
+        
+        resp = rc.CREATED
+        resp['Location'] = os.path.join('/repo', data['destination'], source_arch, rpm)
+        
+        return resp
+
+    def _build_bad_response(self, msg):
+        resp = rc.BAD_REQUEST
+        resp.content = msg
+        return resp
+
---
+++ src/main/python/yum_repo_server/api/test/testRpmPropagation.py
@@ -0,0 +1,44 @@
+from yum_repo_server.test.baseIntegrationTestCase import BaseIntegrationTestCase
+import httplib
+
+class TestRpmPropagation(BaseIntegrationTestCase):
+    
+    def test_propagate_rpm(self):
+        first_repo = self.createNewRepoAndAssertValid()
+        second_repo = self.createNewRepoAndAssertValid()
+        
+        source_rpm = '%s/noarch/test-artifact-1.2-1.noarch.rpm' % first_repo
+        destination_rpm_url = '/repo/%s/noarch/test-artifact-1.2-1.noarch.rpm' % second_repo
+        location_field = self.live_server_url + destination_rpm_url
+        
+        self.repoclient().uploadRpm(first_repo, 'src/test/resources/test-artifact.rpm')
+        
+        response = self.repoclient().propagate_rpm(source_rpm, second_repo)
+        
+        self.assertEquals(location_field, response.getheader('Location'))
+        self.assertEquals(httplib.OK, self.helper.do_http_get(destination_rpm_url).status, 'Could not get the rpm via the expected destination path.')
+
+    def test_rpm_propagation_to_a_non_existent_repository(self):
+        first_repo = self.createNewRepoAndAssertValid()
+        self.repoclient().uploadRpm(first_repo, 'src/test/resources/test-artifact.rpm')
+        
+        post_data = 'source=%s/noarch/test-artifact&destination=not-existing' % first_repo
+        
+        response = self.doHttpPost('/propagation/', post_data)
+        self.assertEquals(httplib.BAD_REQUEST, response.status, 'expected statuscode 400 when destination does not exist.')
+        
+        
+    def test_rpm_propagation_from_not_existing_repository(self):
+        second_repo = self.createNewRepoAndAssertValid()
+        post_data = 'source=not-existing/noarch/rpm-name&destination=%s' % second_repo
+        
+        response = self.doHttpPost('/propagation/', post_data)
+        self.assertEquals(httplib.BAD_REQUEST, response.status, 'expected statuscode 400 when source repository does not exist.')
+        
+    def test_rpm_propagation_with_bad_formatted_source_string(self):
+        second_repo = self.createNewRepoAndAssertValid()
+        post_data = 'source=repo-name/rpm-name&destination=%s' % second_repo
+        
+        response = self.doHttpPost('/propagation/', post_data)
+        self.assertEquals(httplib.BAD_REQUEST, response.status, 'expected statuscode 400 when source string is not correctly formatted.')
+        
