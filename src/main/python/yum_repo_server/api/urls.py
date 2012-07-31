8d370780db076152bbdc9700254223b97c3ca7e6
e-ihrke <ihrke.e@gmail.com>
2012-07-31 15:30:30 +0200

    refactored rpm deletion out of viewer handler.

--- src/main/python/yum_repo_server/api/urls.py
+++ src/main/python/yum_repo_server/api/urls.py
@@ -8,6 +8,7 @@ from yum_repo_server.api.handlers.virtualRepoHandler import VirtualRepoHandler
 from yum_repo_server.api.handlers.virtualRepoConfigHandler import VirtualRepoConfigHandler
 from yum_repo_server.api.handlers.staticRepoHandler import StaticRepoHandler
 from yum_repo_server.api.handlers.csvListingHandler import CsvListingHandler
+from yum_repo_server.api.handlers.RepositoryViewerHandler import RepositoryViewerHandler
 
 
 
@@ -24,6 +25,7 @@ deliverVirtualRepoResource = Resource(handler=VirtualRepoHandler)
 repoConfigHandler = Resource(handler=VirtualRepoConfigHandler)
 staticRepoResource = Resource(handler=StaticRepoHandler)
 repoCsvListingResource = Resource(handler=CsvListingHandler)
+repositoryViewerResource = Resource(handler=RepositoryViewerHandler)
 
 urlpatterns = patterns('',
     url(r'^(?P<repodir>[a-zA-Z0-9-.]+)\.txt',repoCsvListingResource),
@@ -35,8 +37,9 @@ urlpatterns = patterns('',
     url(r'^virtual/(?P<reponame>[a-zA-Z0-9-.]+)(?P<rpm>/[a-zA-Z0-9-./]*)?$', deliverVirtualRepoResource),
     url(r'^(?P<reponame>[a-zA-Z0-9-.]+)/repodata/?$', createYumMetaDataResource),
     url(r'^(?P<reponame>[a-zA-Z0-9-.]+/?)$', uploadToYumRepoHandler),
+    url(r'^(?P<reponame>[a-zA-Z0-9-.]+)/(?P<arch>[a-zA-Z0-9-.]+)/(?P<rpm>[a-zA-Z0-9-.]+)$', staticRepoResource),
     url(r'^(?P<rpm>[a-zA-Z0-9-./]+)/info.html', 'yum_repo_server.api.views.rpm_info_static'),
-    url(r'^(?P<path>.*)$', staticRepoResource),
+    url(r'^(?P<path>.*)$', repositoryViewerResource),
 )
 
 # initialize temp dir for uploads
