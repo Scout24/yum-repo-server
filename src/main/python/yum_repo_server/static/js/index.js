66fab2313f67d3e1511db3847d1caa84321e0476
sherold <sebastian.herold@immobilienscout24.de>
2012-08-02 18:26:49 +0200

    implement unset tag

--- src/main/python/yum_repo_server/static/js/index.js
+++ src/main/python/yum_repo_server/static/js/index.js
@@ -4,43 +4,58 @@ $(document).ready(function() {
 });
 
 window.yum = {
-		saveRepo : function(reponame) {
-			reposelect = $('#reposelect');
-			var destination = reposelect.val();
-			
-			$.ajax({
-				type : 'POST',
-				cache : false,
-				url : '',
-				data : 'name=' + reponame + '&destination=' + destination,
-				success: function () { 
-					$('#reposelect *').removeAttr('selected');
-					$('#reposelect [value= "' + destination + '"]').attr('selected','selected');
-					alert('Saved');
-				},
-				error: function (xhr, status, error) {
-					alert('Saving failed : ' + status);
-				}
-			});
-		},
-		
-		selectRepo : function() {
-			$('#cluetip a.button').show();
-		},
+    saveRepo : function(reponame) {
+        reposelect = $('#reposelect');
+        var destination = reposelect.val();
 
-        addTag : function(tagElem, reponame, tag) {
-          $.ajax({
-                type : 'POST',
-                cache : false,
-                url : reponame + '/tags/',
-                data : 'tag=' + tag,
-                success: function () {
-                    alert('Saved');
-                    window.location.reload();
-                },
-                error: function (xhr, status, error) {
-                    alert('Tagging failed : ' + status);
-                }
-            });
-        }
+        $.ajax({
+            type : 'POST',
+            cache : false,
+            url : '',
+            data : 'name=' + reponame + '&destination=' + destination,
+            success: function () {
+                $('#reposelect *').removeAttr('selected');
+                $('#reposelect [value= "' + destination + '"]').attr('selected','selected');
+                alert('Saved');
+            },
+            error: function (xhr, status, error) {
+                alert('Saving failed : ' + status);
+            }
+        });
+    },
+		
+    selectRepo : function() {
+        $('#cluetip a.button').show();
+    },
+
+    addTag : function(tagElem, reponame, tag) {
+      $.ajax({
+            type : 'POST',
+            cache : false,
+            url : reponame + '/tags/',
+            data : 'tag=' + tag,
+            success: function () {
+                alert('Saved');
+                window.location.reload();
+            },
+            error: function (xhr, status, error) {
+                alert('Tagging failed : ' + status);
+            }
+        });
+    },
+
+    removeTag : function(tagElem, reponame, tag) {
+        $.ajax({
+            type : 'DELETE',
+            cache : false,
+            url : reponame + '/tags/' + tag,
+            success: function () {
+                alert('Saved');
+                window.location.reload();
+            },
+            error: function (xhr, status, error) {
+                alert('Tagging failed : ' + status);
+            }
+        });
+    }
 }
