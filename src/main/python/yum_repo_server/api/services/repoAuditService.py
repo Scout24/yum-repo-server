3c4bbd4ee5f0b614232d9935ce4a02ca6f66698a
Maximilien Riehl <maximilien.riehl@gmail.com>
2012-09-27 17:29:36 +0200

    log remote hostnames in audit

--- src/main/python/yum_repo_server/api/services/repoAuditService.py
+++ src/main/python/yum_repo_server/api/services/repoAuditService.py
@@ -7,6 +7,9 @@ class RepoAuditService(object):
 
   def log_action(self,function_description, request):
        username='<unauthenticated>'
+       host='<unknown>'
        if 'REMOTE_USER' in request.META:
          username=request.META['REMOTE_USER']
-       self.logger.info('User %s %s'%(username,function_description))
+       if 'REMOTE_HOST' in request.META:
+         host=request.META['REMOTE_HOST']
+       self.logger.info('User %s@%s %s'%(username,host,function_description))
