b3a7bf0dbd8fc912f3566f7fd39f643e053e74ea
Michael Gruber <aelgru@gmail.com>
2012-11-22 13:28:57 +0100

    refactoring: first step to extract method _validate_post_data

--- src/main/python/yum_repo_server/api/handlers/rpmPropagationHandler.py
+++ src/main/python/yum_repo_server/api/handlers/rpmPropagationHandler.py
@@ -1,11 +1,13 @@
+import os
+import re
+import shutil
+
 from piston.handler import BaseHandler
 from piston.utils import rc
 from yum_repo_server.api.services.repoConfigService import RepoConfigService
 from yum_repo_server.api.services.repoAuditService import RepoAuditService
-import os
-import shutil
-from yum_repo_server.api.services.rpmService import create_rpm_file_object,\
-    RpmService
+
+from yum_repo_server.api.services.rpmService import create_rpm_file_object, RpmService
 
 class PropagationException(BaseException):
     """
@@ -16,27 +18,32 @@ class PropagationException(BaseException):
 class RpmPropagationHandler(BaseHandler):
     config = RepoConfigService()
     audit = RepoAuditService()
-    
+
+    def _validate_post_data(self, data):
+        if not data.has_key('source') or not data.has_key('destination'):
+            raise PropagationException('source and destination attribute must be set.')
+
+        if not re.match(r".+/.+/.+", data['source']):
+            raise PropagationException('source format is not valid. (repo_name/architecture/rpm-name)')
+
+        destination = data['destination']
+        if '/' in destination or str(destination).startswith('.'):
+            raise PropagationException('destination must not contain slashes.')
+
+
     def create(self, request):
         data = request.POST
 
         try:
-            if not data.has_key('source') or not data.has_key('destination'):
-                raise PropagationException('source and destination attribute must be set.')
+            self._validate_post_data(data)
 
-            try:
-                source_repo, source_arch, rpm = data['source'].split('/')
-            except ValueError:
-                raise PropagationException('source format is not valid. (repo_name/architecture/rpm-name)')
-
+            source_repo, source_arch, rpm = data['source'].split('/')
             source_repo_path = self.config.getStaticRepoDir(source_repo)
 
             if not os.path.exists(source_repo_path):
                 raise PropagationException('source repository does not exist.')
 
             destination = data['destination']
-            if '/' in destination or str(destination).startswith('.'):
-                raise PropagationException('destination must not contain slashes.')
 
             destination_repo_path = self.config.getStaticRepoDir(destination)
             if not os.path.exists(destination_repo_path):
@@ -44,19 +51,18 @@ class RpmPropagationHandler(BaseHandler):
 
             rpm = self._determine_rpm_file_name(os.path.join(source_repo_path, source_arch), rpm)
 
-            if not rpm:
-                raise PropagationException('rpm_file could not be found.')
-
             source_rpm_path = os.path.join(source_repo_path, source_arch, rpm)
             destination_rpm_path = os.path.join(destination_repo_path, source_arch, rpm)
+
             if not os.path.exists(source_rpm_path):
                 raise PropagationException('rpm_file could not be found.')
 
             destination_rpm_parent_dir = os.path.dirname(destination_rpm_path)
+
             if not os.path.exists(destination_rpm_parent_dir):
                 os.mkdir(destination_rpm_parent_dir)
 
-            self.audit.log_action("propagated rpm %s/%s from %s to %s"%(source_arch, rpm, source_repo,destination),request)
+            self.audit.log_action("propagated rpm %s/%s from %s to %s" % (source_arch, rpm, source_repo, destination), request)
             shutil.move(source_rpm_path, destination_rpm_path)
 
             resp = rc.CREATED
@@ -67,11 +73,20 @@ class RpmPropagationHandler(BaseHandler):
         except BaseException as e:
             return self._convert_exception_to_response(e)
 
+
     def _determine_rpm_file_name(self, directory, rpm):
-        if create_rpm_file_object(rpm) is not None:
-            return rpm
-        
-        return RpmService().get_latest_rpm(rpm, directory)
+        result = None
+
+        if create_rpm_file_object(rpm) is None:
+            result = RpmService().get_latest_rpm(rpm, directory)
+        else:
+            result = rpm
+
+        if result is None:
+            raise PropagationException('rpm_file could not be found.')
+
+        return result
+
 
     def _convert_exception_to_response(self, exception):
         """
