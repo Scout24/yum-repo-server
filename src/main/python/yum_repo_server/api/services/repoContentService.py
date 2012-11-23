429101749d162b6c1cdbfb145510c8c0dbcd7632
Michael Gruber <aelgru@gmail.com>
2012-11-23 18:37:21 +0100

    added repoContentService which provides method list_packages

---
+++ src/main/python/yum_repo_server/api/services/repoContentService.py
@@ -0,0 +1,27 @@
+import os
+
+from yum_repo_server.api.services.repoConfigService import RepoConfigService
+
+class RepoContentService(object):
+    """
+        Service to retrieve information about the content of a repository.
+    """
+
+    repoConfigService = RepoConfigService()
+
+    def list_packages(self, repository_name):
+        """
+            @return: a list of tuples (first element is the architecture name, second element is the package file name)
+        """
+        repository_path = self.repoConfigService.getStaticRepoDir(repository_name)
+        available_architectures = os.listdir(repository_path)
+        packages_in_repository = []
+
+        for architecture in available_architectures:
+            architecture_path = os.path.join(repository_path, architecture)
+            packages_in_architecture_dir = os.listdir(architecture_path)
+
+            for package in packages_in_architecture_dir:
+                packages_in_repository.append((architecture, package))
+
+        return packages_in_repository
---
+++ src/main/python/yum_repo_server/api/services/test/testRepoContentService.py
@@ -0,0 +1,111 @@
+import os
+
+from unittest import TestCase
+from mockito import unstub, when, verify, any as any_value
+from yum_repo_server.api.services.repoContentService import RepoContentService
+from yum_repo_server.api.services.repoConfigService import RepoConfigService
+
+import yum_repo_server
+
+class TestRepoContentService(TestCase):
+
+    def tearDown(self):
+        unstub()
+
+    def test_should_return_empty_list_when_no_packages_in_repository(self):
+        repository_path="/path/to/repository"
+        repository = "testrepo"
+        architecture1 = "arch1"
+        architecture2 = "arch2"
+        architecture3 = "arch3"
+
+        when(yum_repo_server.api.services.repoContentService.os).listdir(any_value()).thenReturn([architecture1, architecture2, architecture3]).thenReturn([])
+        when(RepoConfigService).getStaticRepoDir(any_value()).thenReturn(repository_path)
+
+        service = RepoContentService()
+
+        actual_packages = service.list_packages(repository)
+
+        self.assertEqual([], actual_packages)
+
+        verify(RepoConfigService).getStaticRepoDir(repository)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(repository_path)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(os.path.join(repository_path, architecture1))
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(os.path.join(repository_path, architecture2))
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(os.path.join(repository_path, architecture3))
+
+    def test_should_return_one_package_from_archicture_directory(self):
+        repository_path="/path/to/repository"
+        repository = "testrepo"
+        package = "spam.rpm"
+        architecture = "arch1"
+        architecture_path = os.path.join(repository_path, architecture)
+
+        when(yum_repo_server.api.services.repoContentService.os).listdir(repository_path).thenReturn([architecture])
+        when(RepoConfigService).getStaticRepoDir(any_value()).thenReturn(repository_path)
+        when(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path).thenReturn([package])
+
+        service = RepoContentService()
+
+
+        actual_packages = service.list_packages(repository)
+
+
+        self.assertEqual([(architecture, package)], actual_packages)
+
+        verify(RepoConfigService).getStaticRepoDir(repository)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(repository_path)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path)
+
+    def test_should_return_two_packages_from_archicture_directory(self):
+        repository_path="/path/to/repository"
+        repository = "testrepo"
+        package1 = "spam.rpm"
+        package2 = "egg.rpm"
+        packages = [package1, package2]
+        architecture = "arch1"
+        architecture_path = os.path.join(repository_path, architecture)
+
+        when(yum_repo_server.api.services.repoContentService.os).listdir(repository_path).thenReturn([architecture])
+        when(RepoConfigService).getStaticRepoDir(any_value()).thenReturn(repository_path)
+        when(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path).thenReturn(packages)
+
+        service = RepoContentService()
+
+
+        actual_packages = service.list_packages(repository)
+
+
+        self.assertEqual([(architecture, package1), (architecture, package2)], actual_packages)
+
+        verify(RepoConfigService).getStaticRepoDir(repository)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(repository_path)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path)
+
+    def test_should_return_two_packages_from_two_different_architecture_directories(self):
+        repository_path="/path/to/repository"
+        repository = "testrepo"
+        package1 = "spam.rpm"
+        package2 = "egg.rpm"
+        architecture1 = "arch1"
+        architecture_path1 = os.path.join(repository_path, architecture1)
+        architecture2 = "arch2"
+        architecture_path2 = os.path.join(repository_path, architecture2)
+
+        when(yum_repo_server.api.services.repoContentService.os).listdir(repository_path).thenReturn([architecture1, architecture2])
+        when(RepoConfigService).getStaticRepoDir(any_value()).thenReturn(repository_path)
+        when(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path1).thenReturn([package1])
+        when(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path2).thenReturn([package2])
+
+        service = RepoContentService()
+
+
+        actual_packages = service.list_packages(repository)
+
+
+        self.assertEqual([(architecture1, package1), (architecture2, package2)], actual_packages)
+
+        verify(RepoConfigService).getStaticRepoDir(repository)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(repository_path)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path1)
+        verify(yum_repo_server.api.services.repoContentService.os).listdir(architecture_path2)
