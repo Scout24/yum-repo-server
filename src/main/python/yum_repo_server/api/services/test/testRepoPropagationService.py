ca83bf0ca4440158e86f8434c642aa99f963c206
Michael Gruber <aelgru@gmail.com>
2012-11-23 11:04:29 +0100

    added verification of calls

--- src/main/python/yum_repo_server/api/services/test/testRepoPropagationService.py
+++ src/main/python/yum_repo_server/api/services/test/testRepoPropagationService.py
@@ -1,3 +1,5 @@
+import os
+
 from unittest import TestCase
 from mockito import mock, when, verify, any as any_value, unstub
 from yum_repo_server.api.services.repoPropagationService import RepoPropagationService
@@ -17,19 +19,34 @@ class TestRepoPropagationService(TestCase):
 
         source_repository_name = "source-repository-name"
         source_path = "source-static-repository-path"
-        when(mockConfigService).getStaticRepoDir(source_repository_name).thenReturn(source_path)
+        when(service.config).getStaticRepoDir(source_repository_name).thenReturn(source_path)
 
         destination_repository_name = "destination-repository-name"
         destination_path = "destination-static-repository-path"
-        when(mockConfigService).getStaticRepoDir(destination_repository_name).thenReturn(destination_path)
+        when(service.config).getStaticRepoDir(destination_repository_name).thenReturn(destination_path)
 
         when(yum_repo_server.api.services.repoPropagationService.os.path).exists(any_value()).thenReturn(True)
         when(yum_repo_server.api.services.repoPropagationService).create_rpm_file_object(any_value()).thenReturn(True)
         when(yum_repo_server.api.services.repoPropagationService.shutil).move(any_value(), any_value()).thenReturn(None)
 
+        package_name = "package_name"
+        architecture = "architecture"
 
-        actual_file_name = service.propagatePackage("package_name", source_repository_name, destination_repository_name, "architecture_name")
+
+        actual_file_name = service.propagatePackage(package_name, source_repository_name, destination_repository_name, architecture)
 
 
-        self.assertEqual("package_name", actual_file_name)
+        self.assertEqual(package_name, actual_file_name)
 
+        verify(service.config).getStaticRepoDir(source_repository_name)
+        verify(service.config).getStaticRepoDir(destination_repository_name)
+
+        verify(yum_repo_server.api.services.repoPropagationService.os.path).exists(source_path)
+        verify(yum_repo_server.api.services.repoPropagationService.os.path).exists(destination_path)
+
+        verify(yum_repo_server.api.services.repoPropagationService).create_rpm_file_object(package_name)
+
+        source_rpm_path = os.path.join(source_path, architecture, package_name)
+        destination_rpm_parent_dir = os.path.join(destination_path, architecture, package_name)
+
+        when(yum_repo_server.api.services.repoPropagationService.shutil).move(source_rpm_path, destination_rpm_parent_dir)
