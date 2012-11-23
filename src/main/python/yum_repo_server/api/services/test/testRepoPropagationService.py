f27b5f23762658f3971c1cae928b64c16b37f363
Michael Gruber <aelgru@gmail.com>
2012-11-23 11:45:30 +0100

    testing raising of exceptions when repositories do not exist.

--- src/main/python/yum_repo_server/api/services/test/testRepoPropagationService.py
+++ src/main/python/yum_repo_server/api/services/test/testRepoPropagationService.py
@@ -2,7 +2,7 @@ import os
 
 from unittest import TestCase
 from mockito import mock, when, verify, any as any_value, unstub
-from yum_repo_server.api.services.repoPropagationService import RepoPropagationService
+from yum_repo_server.api.services.repoPropagationService import RepoPropagationService, PropagationException
 from yum_repo_server.api.services.repoConfigService import RepoConfigService
 
 import yum_repo_server
@@ -12,7 +12,53 @@ class TestRepoPropagationService(TestCase):
     def tearDown(self):
         unstub()
 
+    def test_should_raise_exception_when_source_repository_does_not_exist(self):
+        package_name = "package_name"
+        architecture = "architecture"
+
+        service = RepoPropagationService()
+        mockConfigService = mock(RepoConfigService)
+        service.config = mockConfigService
+
+        source_repository_name = "source-repository-name"
+        source_path = "source-static-repository-path"
+        when(service.config).getStaticRepoDir(source_repository_name).thenReturn(source_path)
+
+        destination_repository_name = "destination-repository-name"
+        destination_path = "destination-static-repository-path"
+        when(service.config).getStaticRepoDir(destination_repository_name).thenReturn(destination_path)
+
+        when(yum_repo_server.api.services.repoPropagationService.os.path).exists(source_path).thenReturn(False)
+
+        self.assertRaises(PropagationException, service.propagatePackage, package_name, source_repository_name, destination_repository_name, architecture)
+
+
+    def test_should_raise_exception_when_destination_repository_does_not_exist(self):
+        package_name = "package_name"
+        architecture = "architecture"
+
+        service = RepoPropagationService()
+        mockConfigService = mock(RepoConfigService)
+        service.config = mockConfigService
+
+        source_repository_name = "source-repository-name"
+        source_path = "source-static-repository-path"
+        when(service.config).getStaticRepoDir(source_repository_name).thenReturn(source_path)
+
+        destination_repository_name = "destination-repository-name"
+        destination_path = "destination-static-repository-path"
+        when(service.config).getStaticRepoDir(destination_repository_name).thenReturn(destination_path)
+
+        when(yum_repo_server.api.services.repoPropagationService.os.path).exists(source_path).thenReturn(True)
+        when(yum_repo_server.api.services.repoPropagationService.os.path).exists(destination_path).thenReturn(False)
+
+        self.assertRaises(PropagationException, service.propagatePackage, package_name, source_repository_name, destination_repository_name, architecture)
+
+
     def test_should_propagate_package(self):
+        package_name = "package_name"
+        architecture = "architecture"
+
         service = RepoPropagationService()
         mockConfigService = mock(RepoConfigService)
         service.config = mockConfigService
@@ -29,10 +75,6 @@ class TestRepoPropagationService(TestCase):
         when(yum_repo_server.api.services.repoPropagationService).create_rpm_file_object(any_value()).thenReturn(True)
         when(yum_repo_server.api.services.repoPropagationService.shutil).move(any_value(), any_value()).thenReturn(None)
 
-        package_name = "package_name"
-        architecture = "architecture"
-
-
         actual_file_name = service.propagatePackage(package_name, source_repository_name, destination_repository_name, architecture)
 
 
@@ -47,6 +89,10 @@ class TestRepoPropagationService(TestCase):
         verify(yum_repo_server.api.services.repoPropagationService).create_rpm_file_object(package_name)
 
         source_rpm_path = os.path.join(source_path, architecture, package_name)
-        destination_rpm_parent_dir = os.path.join(destination_path, architecture, package_name)
+        destination_rpm_parent_dir = os.path.join(destination_path, architecture)
+        destination_rpm_path = os.path.join(destination_rpm_parent_dir, package_name)
 
-        when(yum_repo_server.api.services.repoPropagationService.shutil).move(source_rpm_path, destination_rpm_parent_dir)
+        verify(yum_repo_server.api.services.repoPropagationService.os.path).exists(source_rpm_path)
+        verify(yum_repo_server.api.services.repoPropagationService.os.path).exists(destination_rpm_parent_dir)
+
+        verify(yum_repo_server.api.services.repoPropagationService.shutil).move(source_rpm_path, destination_rpm_path)
