c13d1f16c8df6374e88f6803f6ed8b61e4e8fc4f
Michael Gruber <aelgru@gmail.com>
2012-11-30 10:23:40 +0100

    refactoring: extracting method list_architectures

--- src/main/python/yum_repo_server/api/services/repoContentService.py
+++ src/main/python/yum_repo_server/api/services/repoContentService.py
@@ -9,29 +9,35 @@ class RepoContentService(object):
 
     repoConfigService = RepoConfigService()
 
+    def list_architectures(self, repository_name):
+        """
+            @return: a dictionary which maps the architecture name to the corresponding repository path
+        """
+        repository_path = self.repoConfigService.getStaticRepoDir(repository_name)
+        files_in_repository = os.listdir(repository_path)
+
+        available_architectures = {}
+
+        for potential_dir in files_in_repository:
+            architecture_path = os.path.join(repository_path, potential_dir)
+            if potential_dir != "repodata" and os.path.isdir(architecture_path):
+                available_architectures[potential_dir] = architecture_path
+
+        return available_architectures
+
     def list_packages(self, repository_name):
         """
             @return: a list of tuples (first element is the architecture name, second element is the package file name)
         """
-        repository_path = self.repoConfigService.getStaticRepoDir(repository_name)
-
-
-        files_in_repository = os.listdir(repository_path)
-        available_architectures = []
-
-        for potential_dir in files_in_repository:
-            if potential_dir != "repodata":
-                if os.path.isdir(os.path.join(repository_path, potential_dir)):
-                    available_architectures.append(potential_dir)
-
+        available_architectures = self.list_architectures(repository_name)
         packages_in_repository = []
 
         for architecture in available_architectures:
-            architecture_path = os.path.join(repository_path, architecture)
+            architecture_path = available_architectures[architecture]
             packages_in_architecture_dir = os.listdir(architecture_path)
 
             for package in packages_in_architecture_dir:
-                package_path = os.path.join(repository_path, architecture, package)
+                package_path = os.path.join(architecture_path, package)
                 packages_in_repository.append((architecture, package_path))
 
         return packages_in_repository
