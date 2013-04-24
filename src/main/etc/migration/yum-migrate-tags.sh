391150d7d75fd1ac4d53231504cc91e9520ae0bb
martingruhn <martin.gruhn@immobilienscout24.de>
2013-04-24 17:30:20 +0200

    fixed migrate-tags script so it properly handles given repo basedir

--- src/main/etc/migration/yum-migrate-tags.sh
+++ src/main/etc/migration/yum-migrate-tags.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 if [ ! -d $1 ] || [ -z $2 ]; then
-	echo "Usage: $0 REPOBASEDIR TARGETHOST [--dry]" ; echo ; exit 0
+	echo "Usage:    yum-migrate-tags.sh REPOBASEDIR TARGETHOST [--dry]"
+	echo
+	echo "Example:  yum-migrate-tags.sh /data/repo/static/ new-yum.yourdomain.com --dry" 
+	echo ; exit 0
 fi
 
 base=$1
 host=$2
-test "$3" == "--dry"
-dry=$?
+dry=$3
 
-repos=$(find $base -name 'tags.txt' | cut -d '/' -f2)
+repos=$(find $base -name 'tags.txt' | rev | cut -d '/' -f2 | rev)
 
 for r in $repos; do 
-	tags=$(cat $r/tags.txt | sort -u)
+	tags=$(cat $base/$r/tags.txt | sort -u)
 	for t in $tags; do
-		if [ $dry -eq 0 ]; then
+		if [ $dry == "--dry" ]; then
 			echo "repoclient --hostname $host tag $r $t"
 		else
 			repoclient --hostname $host tag $r $t
