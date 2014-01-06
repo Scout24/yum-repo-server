162981c7cd5dd102fee30d34ff56753cfbc3b9f9
heroldus <sebastian.herold@immobilienscout24.de>
2014-01-07 00:27:33 +0100

    fix .* import

--- src/main/java/de/is24/infrastructure/gridfs/http/web/filter/AccessLogFilter.java
+++ src/main/java/de/is24/infrastructure/gridfs/http/web/filter/AccessLogFilter.java
@@ -3,7 +3,12 @@ package de.is24.infrastructure.gridfs.http.web.filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
