aa11006058f3a4ca0b8f0b47acc845f7d6b69298
Michael Haslgrübler <michael.haslgruebler@porscheinformatik.at>
2014-07-16 12:17:18 +0200

    added possibility to override configuration.properties file

--- src/main/java/de/is24/infrastructure/gridfs/http/PropertyConfig.java
+++ src/main/java/de/is24/infrastructure/gridfs/http/PropertyConfig.java
@@ -1,16 +1,22 @@
 package de.is24.infrastructure.gridfs.http;
 
-import com.google.common.collect.ImmutableSet;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import com.google.common.collect.ImmutableSet;
+
 
 public class PropertyConfig {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PropertyConfig.class);
   private static final Set<String> PROPERTY_FILES = ImmutableSet.of("configuration.properties", "version.properties");
 
   @Bean
@@ -24,9 +30,17 @@ public class PropertyConfig {
     // if configuration properties exists
 
     for (String propertiesFile : PROPERTY_FILES) {
-      Resource configurationResource = new ClassPathResource(propertiesFile);
+      Resource configurationResource = null;
+      if (System.getProperty(propertiesFile+"File") == null) {
+        configurationResource = new ClassPathResource(propertiesFile);
+      } else {
+        configurationResource = new FileSystemResource(System.getProperty(propertiesFile+"File"));
+        LOGGER.info("file based configuration resource defined with {} ",System.getProperty(propertiesFile+"File"));
+      }
       if (configurationResource.exists()) {
         propertyResources.add(configurationResource);
+      } else {
+          LOGGER.error("error resource {} doesn't exist",configurationResource.toString());
       }
     }
     propertyPlaceholderConfigurer.setLocations(propertyResources.toArray(new Resource[propertyResources.size()]));
