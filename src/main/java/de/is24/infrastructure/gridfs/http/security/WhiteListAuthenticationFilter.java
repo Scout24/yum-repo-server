3e9fb02d649c4020f7279e84b4ceefc6a2d5c417
heroldus <sebastian.herold@immobilienscout24.de>
2014-08-25 14:59:34 +0200

    use basic-auth username as principal if some is given

--- src/main/java/de/is24/infrastructure/gridfs/http/security/WhiteListAuthenticationFilter.java
+++ src/main/java/de/is24/infrastructure/gridfs/http/security/WhiteListAuthenticationFilter.java
@@ -9,10 +9,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+import static java.nio.charset.Charset.forName;
 import static java.util.stream.Collectors.toSet;
+import static org.apache.commons.lang.StringUtils.substringBefore;
 import static org.springframework.util.StringUtils.commaDelimitedListToSet;
 import static org.springframework.util.StringUtils.trimAllWhitespace;
 
@@ -20,6 +23,7 @@ import static org.springframework.util.StringUtils.trimAllWhitespace;
 @ManagedResource
 public class WhiteListAuthenticationFilter extends AbstractPreAuthenticatedProcessingFilter {
   public static final String WHITE_LISTED_HOSTS_MODIFCATION_ENABLED_KEY = "security.whitelist.modification.enabled";
+  public static final String BASIC_AUTH_PREFIX = "Basic ";
 
   private String whiteListedHosts;
   private Set<Pattern> whiteListedHostPatterns;
@@ -40,18 +44,13 @@ public class WhiteListAuthenticationFilter extends AbstractPreAuthenticatedProce
   @Override
   protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
     HostName hostName = hostnameResolver.remoteHost(request);
-    for (Pattern whiteListedHostPattern : whiteListedHostPatterns) {
-      if (whiteListedHostPattern.matcher(hostName.getName()).matches()) {
-        return hostName.getName();
-      }
-    }
-
-    return null;
+    return isWhiteListedHost(hostName) ? getUsernameFrom(request, hostName) : null;
   }
 
   @Override
   protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
-    return getPreAuthenticatedPrincipal(request);
+    HostName hostName = hostnameResolver.remoteHost(request);
+    return isWhiteListedHost(hostName) ? hostName.getName() : null;
   }
 
   @ManagedAttribute
@@ -74,5 +73,25 @@ public class WhiteListAuthenticationFilter extends AbstractPreAuthenticatedProce
     this.whiteListedHosts = whiteListedHosts;
     Set<String> whiteListedHostsStrings = commaDelimitedListToSet(trimAllWhitespace(whiteListedHosts));
     this.whiteListedHostPatterns = whiteListedHostsStrings.stream().map(wildcardToRegexConverter::convert).collect(toSet());
+  }
+
+  protected String getUsernameFrom(HttpServletRequest request, HostName hostName) {
+    final String authorization = request.getHeader("Authorization");
+    if (authorization != null && authorization.startsWith(BASIC_AUTH_PREFIX)) {
+      String base64Credentials = authorization.substring(BASIC_AUTH_PREFIX.length());
+      String credentials = new String(Base64.getDecoder().decode(base64Credentials), forName("UTF-8"));
+      return substringBefore(credentials, ":");
+    }
+
+    return hostName.getName();
+  }
+
+  protected boolean isWhiteListedHost(HostName hostName) {
+    for (Pattern whiteListedHostPattern : whiteListedHostPatterns) {
+      if (whiteListedHostPattern.matcher(hostName.getName()).matches()) {
+        return true;
+      }
+    }
+    return false;
   }
 }
