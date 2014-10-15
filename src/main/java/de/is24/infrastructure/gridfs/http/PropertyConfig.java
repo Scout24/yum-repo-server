56e558cf743bb540a569ef07318ba1f1103ab7f6
heroldus <sebastian.herold@immobilienscout24.de>
2014-10-15 23:18:13 +0200

    According to Pull Request #45 add a possibility to use external configuration properties files via system property: -DconfigurationFile=/path/to/foo.properties,/path/to/another/bar.properties

--- src/main/java/de/is24/infrastructure/gridfs/http/PropertyConfig.java
+++ src/main/java/de/is24/infrastructure/gridfs/http/PropertyConfig.java
@@ -4,14 +4,19 @@ import com.google.common.collect.ImmutableSet;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import static org.springframework.util.StringUtils.commaDelimitedListToSet;
+
 
 public class PropertyConfig {
   private static final Set<String> PROPERTY_FILES = ImmutableSet.of("configuration.properties", "version.properties");
+  private static final String CONFIGURATION_FILE_PROPERTY = "configurationFile";
 
   @Bean
   public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
@@ -19,17 +24,7 @@ public class PropertyConfig {
     propertyPlaceholderConfigurer.setSystemPropertiesMode(
       PropertyPlaceholderConfigurer.SYSTEM_PROPERTIES_MODE_OVERRIDE);
     propertyPlaceholderConfigurer.setSearchSystemEnvironment(true);
-
-    List<Resource> propertyResources = new ArrayList<>();
-    // if configuration properties exists
-
-    for (String propertiesFile : PROPERTY_FILES) {
-      Resource configurationResource = new ClassPathResource(propertiesFile);
-      if (configurationResource.exists()) {
-        propertyResources.add(configurationResource);
-      }
-    }
-    propertyPlaceholderConfigurer.setLocations(propertyResources.toArray(new Resource[propertyResources.size()]));
+    propertyPlaceholderConfigurer.setLocations(getPropertiesFileResources());
 
     // Allow for other PropertyPlaceholderConfigurer instances.
     propertyPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
@@ -37,5 +32,27 @@ public class PropertyConfig {
     // allow to set null value as default in @Value("${any.property:@null}")
     propertyPlaceholderConfigurer.setNullValue("@null");
     return propertyPlaceholderConfigurer;
+  }
+
+  public Resource[] getPropertiesFileResources() {
+    List<Resource> propertyResources = new ArrayList<>();
+
+    for (String propertiesFile : PROPERTY_FILES) {
+      Resource configurationResource = new ClassPathResource(propertiesFile);
+      if (configurationResource.exists()) {
+        propertyResources.add(configurationResource);
+      }
+    }
+
+    if (System.getProperty(CONFIGURATION_FILE_PROPERTY) != null) {
+      for (String propertyFilePath : commaDelimitedListToSet(System.getProperty(CONFIGURATION_FILE_PROPERTY))) {
+        FileSystemResource configurationResource = new FileSystemResource(propertyFilePath);
+        if (configurationResource.exists()) {
+          propertyResources.add(configurationResource);
+        }
+      }
+    }
+
+    return propertyResources.toArray(new Resource[propertyResources.size()]);
   }
 }
